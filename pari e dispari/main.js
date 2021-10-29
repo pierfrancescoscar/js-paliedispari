ec85a23b77a01eb7b43c70ddd8344e9bcf26d57b
pierfrancescoscar <scarinci401@gmail.com>
2021-10-29 16:51:39 +0200

    add user number validator from 1 to 5

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -12,15 +12,27 @@ const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
 console.log(userNumber);
 let cpuRandNumb = randomNumber(1, 5);
 
+// Sum between user entered number and cpu generated random number
+
+
+
+// Entered numbers Validation
+if (userNumber < 1 || userNumber > 5) {
+    alert('Please, enter a number from 1 to 5.');
+}
 
 // Generate a random number from 1 to 5 for CPU:
 
 function randomNumber(min, max) {
     let cpuRandNumb = Math.floor(Math.random() * (max - min + 1) ) + min;
     console.log(cpuRandNumb);
-  }
+}
 
-  
+
+
+
+
+
 
 
 
