80caa8cbffef696e7d40fdf956f61efefa593f7f
pierfrancescoscar <scarinci401@gmail.com>
2021-10-29 17:15:06 +0200

    -fix- add return to function and add sum user-cpu

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -7,12 +7,11 @@
 
 // Ref
 const userOddEven = prompt('Scegli tra pari o dispari');
-// console.log(userOddEven);
 const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
-// console.log(userNumber);
 let cpuRandNumb = randomNumber(1, 5);
 
-// Entered numbers Validation
+
+// Entered numbers validation
 if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
     alert('Please, enter a number from 1 to 5.');
 }
@@ -20,19 +19,13 @@ if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
 // Sum between user entered number and cpu generated random number
 
 let userCpuSum = userNumber + cpuRandNumb;
-console.log(userCpuSum);
-
-
-
 
 // Generate a random number from 1 to 5 for CPU:
 
 function randomNumber(min, max) {
-    let cpuRandNumb = Math.floor(Math.random() * (max - min + 1) ) + min;
-    // console.log(cpuRandNumb);
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    
 }
-
-
 
 
 
