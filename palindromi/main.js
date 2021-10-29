136e6084410ae4b3c46c477002c5b097d6b420aa
pierfrancescoscar <scarinci401@gmail.com>
2021-10-29 15:30:54 +0200

    add "js palindrome" exercise main setup

---
+++ palindromi/main.js
@@ -0,0 +1,13 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Refs
+
+const userWord = prompt('Please, enter a word.');
+// console.log(userWord); //
+
+// Create a function in order to know if the entered word is palindrome
+
+
+
