bcb58ff34c261195b9021c90e5eb0ea5e515a29e
pierfrancescoscar <scarinci401@gmail.com>
2021-10-29 17:43:15 +0200

    -fix- add right position to while valid. loops

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -6,26 +6,35 @@
 // Dichiariamo chi ha vinto.
 
 // Ref
-const userOddEven = prompt('Scegli tra pari o dispari');
-const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
 let cpuRandNumb = randomNumber(1, 5);
 
+// Ref
+let userOddEven = prompt('Choose odd or even');
+
+// Entered "odd" or "even" validator
+while (userOddEven !== 'odd' && userOddEven !== 'even') {
+    alert('Please, choose odd or even.');
+    userOddEven = prompt('Choose odd or even').trim().toLowerCase();
+}
+
+// Ref
+let userNumber = parseInt(prompt('Enter a number from 1 to 5'));
 
 // Entered numbers validation
-if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
+while (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
     alert('Please, enter a number from 1 to 5.');
+    userNumber = parseInt(prompt('Enter a number from 1 to 5'));
 }
 
 // Sum between user entered number and cpu generated random number
-
 let userCpuSum = userNumber + cpuRandNumb;
 
 // Generate a random number from 1 to 5 for CPU:
-
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-    
 }
+
+
 
 
 
