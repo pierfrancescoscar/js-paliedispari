826412890845ea146aba47fc56137717f8d47ea6
pierfrancescoscar <scarinci401@gmail.com>
2021-10-29 16:56:33 +0200

    -fix- add "isNan" to user number validator

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -7,25 +7,29 @@
 
 // Ref
 const userOddEven = prompt('Scegli tra pari o dispari');
-console.log(userOddEven);
+// console.log(userOddEven);
 const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
-console.log(userNumber);
+// console.log(userNumber);
 let cpuRandNumb = randomNumber(1, 5);
+
+// Entered numbers Validation
+if (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
+    alert('Please, enter a number from 1 to 5.');
+}
 
 // Sum between user entered number and cpu generated random number
 
+let userCpuSum = userNumber + cpuRandNumb;
+console.log(userCpuSum);
 
 
-// Entered numbers Validation
-if (userNumber < 1 || userNumber > 5) {
-    alert('Please, enter a number from 1 to 5.');
-}
+
 
 // Generate a random number from 1 to 5 for CPU:
 
 function randomNumber(min, max) {
     let cpuRandNumb = Math.floor(Math.random() * (max - min + 1) ) + min;
-    console.log(cpuRandNumb);
+    // console.log(cpuRandNumb);
 }
 
 
